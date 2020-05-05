b52571eb7188675b710e35af43853124a924feb9
Evgeny <evgeny-bezlepkin@mail.ru>
2020-05-05 22:57:59 +0700

    spring, basic annotation

---
+++ src/main/java/com/example/luv2code/FortuneService.java
@@ -0,0 +1,7 @@
+package com.example.luv2code;
+
+public interface FortuneService {
+
+	public String getFortune();
+	
+}
---
+++ src/main/java/com/example/luv2code/SportConfig.java
@@ -0,0 +1,48 @@
+package com.example.luv2code;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+ @ComponentScan("com.example.luv2code")
+@PropertySource("classpath:sport.properties")
+public class SportConfig {
+	
+	// add support to resolve ${...} properties
+	//
+	// NOTE: THIS IS ONLY REQUIRED FOR OLD VERSIONS OF SPRING: v4.2 and lower
+	//
+	// This is NOT required if using Spring v4.3+
+	//
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer
+					propertySourcesPlaceHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach AND inject dependency
+	@Bean
+	public Coach swimCoach() {
+		SwimCoach mySwimCoach = new SwimCoach(sadFortuneService());
+		
+		return mySwimCoach;
+	}
+	
+}
+
+
+
+
+
+
+
+
